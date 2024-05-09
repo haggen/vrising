066fe3c9701225352071c5b7c59dbdf62e5e1507
Arthur <arthur@corenzan.com>
2024-05-09 17:35:20 -0300

    Fix client's SteamAppId

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ esac
 
 # Must use the game client's application ID to run the server.
 # https://github.com/StunlockStudios/vrising-dedicated-server-instructions/blob/320f73f2ccda3c21ba424b156ae3fa8d54094eba/1.0.x/INSTRUCTIONS.md
-export SteamAppId="1829350"
+export SteamAppId="1604030"
 
 # Start the server.
 # We us xvfb to initialize a virtual display and wine to execute the Windows binary.
